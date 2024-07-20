f2b33bcd9a390ae79b7ad7bad5cc27313a7d17fd
Mahmoud Magdy <mmagdyhafiz2012@gmail.com>
2024-07-20 09:35:25 +0300

    edits

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,7 @@ function App() {
   useEffect(() => {
     setTimeout(() => {
       setIsLoading(false)
-    }, 100)
+    }, 6000)
 
     if (isLoading) {
       document.body.classList.add('disable-scroll')
