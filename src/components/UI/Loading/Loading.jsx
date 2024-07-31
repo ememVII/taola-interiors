a99a6f5e90d0b3f79f7c771e4506f2d660c582b3
Mahmoud Magdy <mmagdyhafiz2012@gmail.com>
2024-07-31 03:18:15 +0300

    projects images

--- src/components/UI/Loading/Loading.jsx
+++ src/components/UI/Loading/Loading.jsx
@@ -4,7 +4,7 @@ import styles from './Loading.module.css'
 function Loading() {
   return (
     <div className={styles.loading}>
-      <video autoPlay loop muted>
+      <video autoPlay muted>
         <source src="videos/loading.mp4" type="video/mp4" />
         Your browser does not support the video tag.
       </video>
